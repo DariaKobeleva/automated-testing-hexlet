9ec65665c5b05f87d0f26f69c685d2d359b59119
KDaria <dkdva@mail.ru>
2023-01-16 15:34:43 +0100

    Add a new strings

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,12 +1,8 @@
+import { strict as assert } from 'node:assert';
 import { capitalize } from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
-
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize(''), '');
+assert.equal(capitalize('hello world!'), 'Hello world!');
 
 console.log('Все тесты пройдены!');
 
